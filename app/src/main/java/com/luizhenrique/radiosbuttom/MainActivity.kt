0838b10405b7b797ef44e300388e03af6f91ba42
Luiz Henrique <149181864+LuizHenriqueOlv@users.noreply.github.com>
2025-04-17 11:36:57 -0300

    término

--- app/src/main/java/com/luizhenrique/radiosbuttom/MainActivity.kt
+++ app/src/main/java/com/luizhenrique/radiosbuttom/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.luizhenrique.radiosbuttom
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.luizhenrique.radiosbuttom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(R.layout.activity_main)
+        binding.radioGroup.setOnCheckedChangeListener{_,id ->
+            when(id){
+            R.id.radioBtnNotifica ->{
+                Log.i("INFOTESTE", "Opção 1")
+            }
+            R.id.radioBtnNotifica2 ->{
+                Log.i("INFOTESTE", "Opção 2")
+            }
+            R.id.radioBtnNotifica3 ->{
+                Log.i("INFOTESTE", "Opção 3")
+            }
+
+            }
+        }
+
+
 
 
     }
